9c0e163a7a4c6eb78f76359f0ed1fd032ef81434
Vladimir Solo <freeman.wushu@mail.ru>
2025-01-19 21:49:32 +0300

    chore: dotenv install add config for sepolia testnet

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,6 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "./tasks/task"
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
+const ALCHEMY_SEPOLIA_KEY = process.env.ALCHEMY_SEPOLIA_KEY || "";
+const API_URL = `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_SEPOLIA_KEY}`;
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -10,6 +17,16 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 2000
       }
+    }
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      initialBaseFeePerGas: 0,
+    },
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
     }
   },
   // https://github.com/cgewecke/hardhat-gas-reporter --> example usage
