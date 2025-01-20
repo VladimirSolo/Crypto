6e196427bcf5eb1d33b995cb93c7e8412c9af73e
Vladimir Solo <freeman.wushu@mail.ru>
2025-01-20 22:09:23 +0300

    chore: hardhat-verify install and add config

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 import "./tasks/task"
 import dotenv from 'dotenv';
 
@@ -8,6 +9,7 @@ dotenv.config();
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
 const ALCHEMY_SEPOLIA_KEY = process.env.ALCHEMY_SEPOLIA_KEY || "";
 const API_URL = `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_SEPOLIA_KEY}`;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -28,6 +30,17 @@ const config: HardhatUserConfig = {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`]
     }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    // Write on https://repo.sourcify.dev/contracts
+    enabled: true
   },
   // https://github.com/cgewecke/hardhat-gas-reporter --> example usage
   gasReporter: {
