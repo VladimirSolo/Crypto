6820c2e73f3d37a9bb110e346a4c1df5289f2d20
Vladimir Solo <freeman.wushu@mail.ru>
2025-01-20 22:10:08 +0300

    feat: add verify to deploy script album store contract

--- backend/scripts/deployAlbumStore.ts
+++ backend/scripts/deployAlbumStore.ts
@@ -1,17 +1,30 @@
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
 
 async function main() {
   console.log('Process deploing...')
 
-  const [owner, customer] = await ethers.getSigners();
+  const [owner] = await ethers.getSigners();
+  console.log("🚀 owner -> deploed:", owner)
 
   const AlbumStoreFactory = await ethers.getContractFactory('AlbumStore');
   const store = await AlbumStoreFactory.deploy(owner.address);
   await store.waitForDeployment()
 
-  const contractAddress = await store.getAddress()
+  const contractAddress = await store.getAddress();
 
-  console.log(`Deployed to ${contractAddress}`)
+  console.log(`Deployed to ${contractAddress}`);
+
+  // Verifying contract
+  try {
+    console.log("Verifying contract...");
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: [owner.address],
+    });
+    console.log("Verification successful!");
+  } catch (error) {
+    console.error("Verification failed:", error);
+  }
 }
 
 main()
